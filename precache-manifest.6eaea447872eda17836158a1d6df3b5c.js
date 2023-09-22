32a78e0eec8125f0202464281ae75199a4892245
rodrigueaime <rodrigue.aime@yahoo.com>
2023-09-22 19:40:44 +0200

    Updates

---
+++ precache-manifest.6eaea447872eda17836158a1d6df3b5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acdcece50d15bff2fe84ab6505d1c55b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "933d731138c6f705e480",
+    "url": "/portfolio/static/css/main.4194cca1.chunk.css"
+  },
+  {
+    "revision": "56e44704864c0bfe6b3b",
+    "url": "/portfolio/static/js/2.f212301d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.f212301d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933d731138c6f705e480",
+    "url": "/portfolio/static/js/main.7117c5a4.chunk.js"
+  },
+  {
+    "revision": "65bd4987f31e81c4403d",
+    "url": "/portfolio/static/js/runtime-main.a0093325.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/portfolio/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/portfolio/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
